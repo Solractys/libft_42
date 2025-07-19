95a7efcc7f414a3554539f847322d3114c9bdac8
Carlos Eduardo Silva Souza De Lima <csilva-s@c2r12p1.42sp.org.br>
2025-07-19 19:17:00 -0300

    Done

--- libft/src/ft_strrchr.c
+++ libft/src/ft_strrchr.c
@@ -1,42 +1,43 @@
-// /* ************************************************************************** */
-// /*                                                                            */
-// /*                                                        :::      ::::::::   */
-// /*  ft_strrchr.c                                       :+:      :+:    :+:   */
-// /*                                                    +:+ +:+         +:+     */
-// /*   By: csilva-s <marvin@42.fr>                    +#+  +:+       +#+        */
-// /*                                                +#+#+#+#+#+   +#+           */
-// /*   Created: 2025/07/17 14:04:00 by csilva-s          #+#    #+#             */
-// /*   Updated: 2025/07/17 14:04:00 by csilva-s         ###   ########.fr       */
-// /*                                                                            */
-// /* ************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*  ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 14:04:00 by csilva-s          #+#    #+#             */
+/*   Updated: 2025/07/17 14:04:00 by csilva-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-// #include "libft.h"
+#include "libft.h"
 
-// char *ft_strrchr(const char *s, int c);
+char *ft_strrchr(const char *s, int c);
 
-// char *ft_strrchr(const char *s, int c)
-// {
-// 	char	*cpy_c;
-// 	size_t	len_s;
-// 	char	*result;
-// 	short	count;
+char *ft_strrchr(const char *s, int c)
+{
+	char	*cpy_c;
+	size_t	len_s;
+	char	*result;
+	short	count;
 
-// 	cpy_c = (char *)s;
-// 	count = 0;
-// 	len_s = ft_strlen(s);
-// 	while (cpy_c[len_s] != '\0')
-// 	{
-// 		if(cpy_c[len_s] == c)
-// 		{
-// 			while (cpy_c[len_s != '\0'])
-// 				result[count++] = cpy_c[len_s++];
-// 			result[count] = '\0';
-// 			return (result);
-// 		}
-// 		len_s--;
-// 	}
-// 	return (NULL);
-// }
+	cpy_c = (char *)s;
+	result = "";
+	count = 0;
+	len_s = ft_strlen(s);
+	while (cpy_c[len_s] != '\0')
+	{
+		if(cpy_c[len_s] == c)
+		{
+			while (cpy_c[len_s != '\0'])
+				result[count++] = cpy_c[len_s++];
+			result[count] = '\0';
+			return (result);
+		}
+		len_s--;
+	}
+	return (NULL);
+}
 // #include <stdio.h>
 
 // int	main(void)
