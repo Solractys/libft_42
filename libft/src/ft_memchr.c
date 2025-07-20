db6daecc8e9e302f9227aeba31bc679d57f08b28
Carlos Eduardo Silva Souza De Lima <csilva-s@c2r7p3.42sp.org.br>
2025-07-20 18:35:16 -0300

    Done

--- libft/src/ft_memchr.c
+++ libft/src/ft_memchr.c
@@ -32,10 +32,3 @@ void *ft_memchr(const void *s, int c, size_t n)
 	}
 	return (NULL);
 }
-// #include <stdio.h>
-// int	main(void)
-// {
-// 	int	tab[7] = {-49, 49, 1 ,-1, 0, -2, 2};
-// 	printf("%s", (char *)ft_memchr(tab, -1, 7));
-// 	return (0);
-// }
