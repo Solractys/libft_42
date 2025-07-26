cf2ec41bc820556954af64bc69366ca528e93255
Solractys <carloseduardosilvasouzadelima@gmail.com>
2025-07-26 15:25:12 -0300

    Done

---
+++ libft/src/ft_strmapi.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/26 14:42:49 by csilva-s          #+#    #+#             */
+/*   Updated: 2025/07/26 15:18:10 by csilva-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*s2;
+
+	i = 0;
+	s2 = ft_calloc(sizeof(char), ft_strlen(s) + 1);
+	while (i < ft_strlen(s))
+	{
+		s2[i] = f(i, s[i]);
+		i++;
+	}
+	return (s2);
+}
