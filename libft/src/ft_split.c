b59fee61f5b98ebfb9bf6fcaf4d198f90920fd72
Solractys <carloseduardosilvasouzadelima@gmail.com>
2025-07-28 13:48:11 -0300

    Done

--- libft/src/ft_split.c
+++ libft/src/ft_split.c
@@ -6,14 +6,23 @@
 /*   By: csilva-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 13:17:04 by csilva-s          #+#    #+#             */
-/*   Updated: 2025/07/27 15:54:52 by csilva-s         ###   ########.fr       */
+/*   Updated: 2025/07/28 12:00:39 by csilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft.h"
+#include "libft.h"
 
 char	**ft_split(char const *s, char c);
-int	word_count(char const *s, char c);
+char	*ft_alloc_word(char *s, char c);
+void	free_all(char **s, int size);
+int		word_count(char const *s, char c);
+
+void	free_all(char **s, int size)
+{
+	while (size > 0)
+		free(s[size--]);
+	free(s);
+}
 
 int	word_count(char const *s, char c)
 {
@@ -28,7 +37,7 @@ int	word_count(char const *s, char c)
 	{
 		if (s[i] == c)
 		{
-			flag = -1;
+			flag = 1;
 			i++;
 		}
 		else if (s[i] != c)
@@ -44,12 +53,49 @@ int	word_count(char const *s, char c)
 	return (count);
 }
 
+char	*ft_alloc_word(char *s, char c)
+{
+	char	*dest;
+	short	i;
+
+	i = 0;
+	while (s[i] != c && s[i] != '\0')
+		i++;
+	dest = ft_calloc(i + 1, sizeof(char));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s[i] != c && s[i] != '\0')
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	int	words;
 	char	**result;
+	int		words;
+	int		i;
+	int		j;
 
+	i = 0;
+	j = 0;
 	words = word_count(s, c);
-	result = (char **)malloc(sizeof(char *) * words + 1);
-
+	result = (char **)ft_calloc(sizeof(char *), words + 1);
+	if (!result)
+		return (free_all(result, words), NULL);
+	while (s[i] != '\0')
+	{
+		if (s[i] != c)
+		{
+			result[j++] = ft_alloc_word((char *)&s[i], c);
+			while (s[i] != c && s[i] != '\0')
+				i++;
+		}
+		else
+			i++;
+	}
+	return (result);
 }
